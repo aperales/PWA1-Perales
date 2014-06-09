3cadd4394ff0d17481af6097d59e5bf3f0c8e09b
aperales <albertoperales@Albertos-MacBook-Pro.local>
2014-06-08 19:34:50 -0600

    finished code writing

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -5,9 +5,9 @@
  */
 
 //self executing function
-(function(){
+(function() {
 
-  console.log("FIGHT!!!");
+    console.log("FIGHT!!!");
 //player name
     var playerOneName = "Batman";
     var playerTwoName = "Spiderman";
@@ -20,36 +20,71 @@
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
-    var round = 1;
+    var round = 0;
 
-    function fight(){
+    function fight() {
         console.log('in the fight function');
-    alert(playerOneName+"."+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth)
+        alert(playerOneName + "." + playerOneHealth + " *START* " + playerTwoName + ":" + playerTwoHealth);
 
-    for (var i=0; i < 10; i++){
-         //RANDOM FORMULA IS Math.floor(Math.random() * (max - min) + min);
-     var minDamage1 = player1Damage * .5;
-     var minDamage2 = player2Damage * .5;
-     var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-     var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+        for (var i = 0; i < 10; i++) {
+            //RANDOM FORMULA IS Math.floor(Math.random() * (max - min) + min);
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
+            var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
 
-    //console.log(f1);
-    //console.log(f2);
+            //console.log(f1);
+            //console.log(f2);
 
-     };
+            //inflict damage
+            playerOneHealth -= f1;
+            playerTwoHealth -= f2;
+
+            // console.log(playerOneHealth);
+            //console.log(playerTwoHealth);
+            console.log(playerOneName + ":" + playerOneHealth + "" + playerTwoName + ":" + playerTwoHealth);
+//check for victor
+            var results = winnerCheck();
+            console.log(results);
+
+            if (results === "no winner")
+            {
+                round++;
+                alert(playerOneName + "." + playerOneHealth + " *ROUND* " + round + "*OVER*" + playerTwoName + ":" + playerTwoHealth);
+
+            } else {
+                alert(results);
+                break;
+
+            };
+
+
+
+        };
+
+
+    };
+
+
+    function winnerCheck(){
+        console.log("in winnerCheck FN");
+    var result = "no winner";
+    if (playerOneHealth < 1 && playerTwoHealth < 1)
+    {
+        result = "You Both Die";
+    } else if (playerOneHealth < 1){
+        result = playerTwoName + "WINS!!!"
+    } else if (playerTwoHealth < 1)
+    {
+        result = playerOneName + "WINS!!!"
+    };
+    return result;
 
   };
 
 
-
-    function winnerCheck(){
-        //code will go here
-    };
-
-
-
     //Program starts
-console.log('program start')
+        console.log('program start')
     fight();
 
 
