8db8ade9f57473d9e92b5d91d851a12f8bacae3d
aperales <albertoperales@Albertos-MacBook-Pro.local>
2014-06-08 18:31:40 -0600

    Code for duel writing
    continued to follow directions and write code

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -9,8 +9,8 @@
 
   console.log("FIGHT!!!");
 //player name
-    var playerOneName = "Kratos";
-    var playerTwoName = "Smoke";
+    var playerOneName = "Batman";
+    var playerTwoName = "Spiderman";
 
 //player damage
     var player1Damage = 20;
@@ -21,8 +21,36 @@
     var playerTwoHealth = 100;
 
     var round = 1;
-    
 
+    function fight(){
+        console.log('in the fight function');
+    alert(playerOneName+"."+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth)
+
+    for (var i=0; i < 10; i++){
+         //RANDOM FORMULA IS Math.floor(Math.random() * (max - min) + min);
+     var minDamage1 = player1Damage * .5;
+     var minDamage2 = player2Damage * .5;
+     var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+     var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+    //console.log(f1);
+    //console.log(f2);
+
+     };
+
+  };
+
+
+
+    function winnerCheck(){
+        //code will go here
+    };
+
+
+
+    //Program starts
+console.log('program start')
+    fight();
 
 
 })();
